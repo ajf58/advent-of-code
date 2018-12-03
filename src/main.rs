e1abffa8fc82f009a98e88dce9c561ac11ee9b78
Andrew Featherstone <andrew.featherstone@gmail.com>
2018-12-03 08:51:24 +0000

    Day one, part one.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+use std::io::{BufRead, BufReader};
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let filename = &args[1];
+    let f = File::open(filename).unwrap();
+    let f = BufReader::new(f);
+
+    // Iterate over each line, parsing each line as a signed int, and sum.
+    let result: i32 = f.lines().map(|line| line.unwrap().parse::<i32>().unwrap()).sum();
+    println!("Result {}", result);
 }
