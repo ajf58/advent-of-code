beda3b7a737df832748c9c46cbf2051be39b667d
Andrew Featherstone <andrew.featherstone@gmail.com>
2020-12-28 17:41:17 +0000

    Add day 2.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,37 @@ use std::io::{BufRead, BufReader};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    day01(&args);
+    day02(&args);
+}
+
+fn day02(args: &Vec<String>) {
+    let filename = &args[1];
+    let f = File::open(filename).unwrap();
+    let f = BufReader::new(f);
+
+    let mut policy_a = 0;
+    let mut policy_b = 0;
+    // Split the line into the policy and password. Example line:
+    // 1-3 a: abcde
+    for line in f.lines() {
+        let s = line.unwrap();
+        let v: Vec<&str> = s.split_whitespace().collect();
+        let range: Vec<u32> = v[0].split('-')
+            .map(|x| x.parse::<u32>().unwrap())
+            .collect();
+        let min = &range[0];
+        let max = &range[1];
+        let policy_char = v[1].chars().nth(0).unwrap();
+        let num_chars: u32 = v[2].chars().filter(|x| *x == policy_char).count() as u32;
+        
+        if (min <= &num_chars) && (&num_chars <= max) {
+            policy_a += 1;
+        }
+        if (v[2].chars().nth((*min - 1) as usize).unwrap() == policy_char) ^ (v[2].chars().nth((*max - 1) as usize).unwrap() == policy_char) {
+            policy_b += 1;
+        }
+    }
+    println!("Result {:?} {:?}", policy_a, policy_b);
 }
 
 fn day01(args: &Vec<String>) {
@@ -16,12 +46,12 @@ fn day01(args: &Vec<String>) {
     let expenses: Vec<u32> = f.lines()
         .map(|line| line.unwrap().parse::<u32>().unwrap())
         .collect();
-    part01(&expenses);
-    part02(&expenses);
+    day01part01(&expenses);
+    day01part02(&expenses);
 }
 
 // find the two entries that sum to 2020, and multiply together.
-fn part01(expenses: &Vec<u32>) {
+fn day01part01(expenses: &Vec<u32>) {
     let mut result = 0;
     for expense_a in expenses {
         for expense_b in expenses {
@@ -38,7 +68,7 @@ fn part01(expenses: &Vec<u32>) {
 }
 
 // find the three entries that sum to 2020, and multiply together.
-fn part02(expenses: &Vec<u32>) {
+fn day01part02(expenses: &Vec<u32>) {
     let mut result = 0;
     for expense_a in expenses {
         for expense_b in expenses {
